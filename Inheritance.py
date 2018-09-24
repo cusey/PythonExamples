861cbcb14269b0a1b510f3c43aefd73d45018650
John Cusey <jjcusey@gmail.com>
2018-09-24 12:06:55 -0400

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,29 @@
+#python 3.5.2
+
+class Employee(object):
+   
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+class PartTimeEmployee(Employee):
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 12.00
+        
+    def full_time_wage(self, hours):
+        return super(PartTimeEmployee, self).calculate_wage(hours)
+        
+        
+milton = PartTimeEmployee("Milton")
+print( milton.full_time_wage(10) )
+
+
+'''
+OUTPUT:
+
+200.0
