44a41ae6aa32b897459d54042b1278460e690941
John Cusey <jjcusey@gmail.com>
2020-04-19 18:10:54 -0400

    Update AsteriskAsteriskKwargs.py

--- AdvanceTopics/AsteriskAsteriskKwargs.py
+++ AdvanceTopics/AsteriskAsteriskKwargs.py
@@ -7,24 +7,7 @@ def intro(**data):
         print("{} is {}".format(key,value))
 
 intro(Firstname="Sita", Lastname="Sharma", Age=22, Phone=1234567890)
+print( '-'*20 )
+
 intro(Firstname="John", Lastname="Wood", Email="johnwood@nomail.com", Country="Wakanda", Age=25, Phone=9876543210)
-
-
-'''
-OUTPUT:
-
-Data type of argument: <class 'dict'>
-Lastname is Sharma
-Age is 22
-Firstname is Sita
-Phone is 1234567890
-
-Data type of argument: <class 'dict'>
-Email is johnwood@nomail.com
-Country is Wakanda
-Lastname is Wood
-Age is 25
-Firstname is John
-Phone is 9876543210
-
-'''
+print( '-'*20 )
