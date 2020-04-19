9c86450869219629860cbcd98e0b61854c225d1a
John Cusey <jjcusey@gmail.com>
2020-04-19 16:26:08 -0400

    Update BuildingIterator.py

--- List/BuildingIterator.py
+++ List/BuildingIterator.py
@@ -10,9 +10,13 @@ class PowTwo:
         return self
 
     def __next__(self):
+        print("1) n:" + str(self.n) + " max:" + str(self.max))
         if self.n <= self.max:
             result = 2 ** self.n
+            print("2) result:" + str(result) )
+
             self.n += 1
+            print("3) n:" + str(self.n))
             return result
         else:
             raise StopIteration
@@ -21,5 +25,3 @@ class PowTwo:
 a = PowTwo(4)
 i = iter(a)
 print(next(i))
-
-print('-' * 20)
