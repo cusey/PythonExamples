5cf7bcd4f3aa6726433f91c96710b436d5702f52
John Cusey <jjcusey@gmail.com>
2020-04-19 21:43:37 -0400

    Update Insert.py

--- Array/Insert.py
+++ Array/Insert.py
@@ -5,3 +5,12 @@ fruits = ['apple', 'banana', 'cherry']
 fruits.insert(1, "orange")
 
 print( fruits )
+print ( '-'*30)
+
+# Array can have strings and numbers
+alphanumeric = ['E',1,'E',2]
+
+print( alphanumeric)
+print( alphanumeric[1] + 1)
+print ( '-'*30)
+
