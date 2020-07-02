330f5c4aca548e85f341a3365af4d209291b9699
John Cusey <john.cusey@cisa.dhs.gov>
2020-07-02 13:25:23 -0400

    update

--- List/ListComprehensions.py
+++
@@ -1,8 +0,0 @@
-#python 3.5.2
-
-print ( [(x, y) for x in [1,2,3] for y in [3,1,4] if x != y] )
-print(  '-'*20)
-
-print( [i**2 for i in range(1,12) if i % 2 == 0 ] )
-print(  '-'*20)
-
--- Number/Range.py
+++
@@ -1,6 +0,0 @@
-'''
-Create a sequence of numbers from 3 to 11, but increment by 2 instead of 1:
-'''
-x = range(3, 11, 2)
-for n in x:
-  print(n)
